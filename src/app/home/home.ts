0a5acbab2ca389fa159d98494259bfa5171f2eb6
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-10 17:07:26 +0100

    add footer

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Footer} from './footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, Footer],
   templateUrl: './home.html',
   styleUrl: './home.sass',
 })
