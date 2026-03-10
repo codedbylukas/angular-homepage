546fddf082abf6f530c0566400f498e92ac20e7a
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-10 16:14:49 +0100

    add font

---
+++ src/app/data-security/home-btn/home-btn.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeBtn } from './home-btn';
+
+describe('HomeBtn', () => {
+  let component: HomeBtn;
+  let fixture: ComponentFixture<HomeBtn>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HomeBtn]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HomeBtn);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/data-security/home-btn/home-btn.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-btn',
+  imports: [],
+  templateUrl: './home-btn.html',
+  styleUrl: './home-btn.sass',
+})
+export class HomeBtn {
+
+}
