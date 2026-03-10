3014e5294ca808abf4f45f8594dbd5cf0b5e0d32
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-10 13:25:48 -0400

    generate box

---
+++ src/app/home/box/box.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box',
+  imports: [],
+  templateUrl: './box.html',
+  styleUrl: './box.sass',
+})
+export class Box {
+
+}
