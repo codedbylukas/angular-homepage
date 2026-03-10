b1b8fddba5190efda51b5ab7981844fcaa7fd5fd
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-10 17:27:04 +0100

    fixed datasec-link

--- src/app/home/footer/footer.ts
+++ src/app/home/footer/footer.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './footer.html',
   styleUrl: './footer.sass',
 })
