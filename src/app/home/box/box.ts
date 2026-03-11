6e185161386ca945e83624bda63944b0def4286f
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-11 11:54:28 -0400

    fixed bag

--- src/app/home/box/box.ts
+++ src/app/home/box/box.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-box',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterModule],
   templateUrl: './box.html',
   styleUrl: './box.sass',
 })
@@ -11,10 +13,12 @@ export class Box {
     document.getElementById("headline")!.innerHTML = "Hallo, ich bin Lukas";
     document.getElementById("project_headline")!.innerHTML = "Coole Webseiten";
     document.getElementById("platforms")!.innerHTML = "Plattformen";
+    document.getElementById("sand-site-link")!.innerHTML = "Sand Spiel";
   }
   translateToEnglish() {
     document.getElementById("headline")!.innerHTML = "Hello, I'm Lukas";
     document.getElementById("project_headline")!.innerHTML = "Cool Websites";
     document.getElementById("platforms")!.innerHTML = "Platforms";
+    document.getElementById("sand-site-link")!.innerHTML = "Sand Game";
   }
 }
