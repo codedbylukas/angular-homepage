e0cfad5998e2f0c2d962575e38c531e1ed3d91da
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-11 11:47:46 -0400

    add sand site component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home';
 import { DataSecurityComponent } from './data-security/data-security';
+import { SandSite } from './sand-site/sand-site';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'data-security', component: DataSecurityComponent }
+  { path: 'data-security', component: DataSecurityComponent },
+  { path: 'sand-site', component: SandSite },
 ];
---
+++ src/app/sand-site/sand-site.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sand-site',
+  imports: [],
+  templateUrl: './sand-site.html',
+  styleUrl: './sand-site.sass',
+})
+export class SandSite {
+
+}
