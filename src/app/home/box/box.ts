ebf29fdfaf73d40d7b1ff25f9782aac6409da350
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-11 11:44:29 -0400

    better translation

--- src/app/home/box/box.ts
+++ src/app/home/box/box.ts
@@ -9,8 +9,12 @@ import { Component } from '@angular/core';
 export class Box {
    translateToGerman() {
     document.getElementById("headline")!.innerHTML = "Hallo, ich bin Lukas";
+    document.getElementById("project_headline")!.innerHTML = "Coole Webseiten";
+    document.getElementById("platforms")!.innerHTML = "Plattformen";
   }
   translateToEnglish() {
     document.getElementById("headline")!.innerHTML = "Hello, I'm Lukas";
+    document.getElementById("project_headline")!.innerHTML = "Cool Websites";
+    document.getElementById("platforms")!.innerHTML = "Platforms";
   }
 }
