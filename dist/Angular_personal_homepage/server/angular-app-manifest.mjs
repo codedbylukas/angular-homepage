da5ed8c40a4643130117d03dc58b740865a35c28
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-12 15:36:04 -0400

    build android app

--- dist/Angular_personal_homepage/server/angular-app-manifest.mjs
+++ dist/Angular_personal_homepage/server/angular-app-manifest.mjs
@@ -20,11 +20,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 4845, hash: 'c8fb68dfbe34b79b11de0b6bbfd8b292e01832ff62f1846541591da52c4f56a3', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1012, hash: '310443de3ead212fe5f4210b440ff7d6cbc971ec15ee35fa2748cd8692a5b301', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'data-security/index.html': {size: 6891, hash: '0292bbe011569fe76ea69438b9cef68c80aeb0141abb7e0c60ea74804d535648', text: () => import('./assets-chunks/data-security_index_html.mjs').then(m => m.default)},
-    'sand-site/index.html': {size: 6402, hash: '0acea24db7e8bcdf4ca8639286406d413a6e310f1173b66ad44c70daac09bbfa', text: () => import('./assets-chunks/sand-site_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 9317, hash: '38e6c83e05c7fa872e0f4d79ee17893a8a8848064e3893f97aee0e7a6d8f3f7d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 4845, hash: 'a0c703b53b9d83c0390352c7f9ee696693232b6c208a3d6b004b63a683bab42d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1012, hash: '8cc84e0c34b2261958ab96b500b17f41ffee14954975cb4d15c7f710972534d3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 9356, hash: '6a33998b46a2cebfed748f8cfb770e3acb2ebe4b6c5e6f8408531946af4845d5', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'data-security/index.html': {size: 6891, hash: 'b9c41d6f6642627cc09424974e37c7e69cad55a6d8793f13bd8f88df9c362545', text: () => import('./assets-chunks/data-security_index_html.mjs').then(m => m.default)},
+    'sand-site/index.html': {size: 7002, hash: '5678ca5e9c73579add5ea9cb347cbc2df1b939726bb317f25d83a56cd06395a1', text: () => import('./assets-chunks/sand-site_index_html.mjs').then(m => m.default)},
     'styles-WKIBCZ2A.css': {size: 4221, hash: 'X1vJgGzT4bM', text: () => import('./assets-chunks/styles-WKIBCZ2A_css.mjs').then(m => m.default)}
   },
 };
