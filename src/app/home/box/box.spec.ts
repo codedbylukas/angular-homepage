81d9f1bf40bcf601ef408426ab4d2d558047b055
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-12 13:38:23 -0400

    add more test translate

--- src/app/home/box/box.spec.ts
+++ src/app/home/box/box.spec.ts
@@ -22,4 +22,25 @@ describe('Box', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('Translate English is defined', () => {
+    expect(component.translateToEnglish).toBeDefined();
+    expect(typeof component.translateToEnglish).toEqual('function');
+  });
+  it('Translate German is defined', () => {
+    expect(component.translateToGerman).toBeDefined();
+    expect(typeof component.translateToGerman).toEqual('function');
+  });
+  it('should call translateToEnglish when English button clicked', () => {
+    spyOn(component, 'translateToEnglish');
+    const button = fixture.nativeElement.querySelector('#english-button');
+    button.click();
+    expect(component.translateToEnglish).toHaveBeenCalled();
+  });
+  it('should call translateToGerman when German button clicked', () => {
+    spyOn(component, 'translateToGerman');
+    const button = fixture.nativeElement.querySelector('#german-button');
+    button.click();
+    expect(component.translateToGerman).toHaveBeenCalled();
+  });
+  
 });
