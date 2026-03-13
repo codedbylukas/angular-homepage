9bf1d20fb9b66e67b577307bc382d75a5ae00d99
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-13 13:19:38 -0400

    add fun-facts

---
+++ src/app/fun-facts/fun-facts.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fun-facts',
+  imports: [],
+  templateUrl: './fun-facts.html',
+  styleUrl: './fun-facts.sass',
+})
+export class FunFacts {
+
+}
