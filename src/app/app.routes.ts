4edeb51d805c0e374f75d820423b37be16033476
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-13 13:20:52 -0400

    routed it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home';
 import { DataSecurityComponent } from './data-security/data-security';
 import { SandSite } from './sand-site/sand-site';
+import { FunFacts } from './fun-facts/fun-facts';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'data-security', component: DataSecurityComponent },
   { path: 'sand-site', component: SandSite },
+  { path: 'fun-facts', component: FunFacts}
 ];
