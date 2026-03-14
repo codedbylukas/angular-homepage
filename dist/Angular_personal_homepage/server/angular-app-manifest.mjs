1ba1904ccb1c1edab07fb07ecaf99d8a91a718bc
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-14 02:32:41 -0400

    build android app

--- dist/Angular_personal_homepage/server/angular-app-manifest.mjs
+++ dist/Angular_personal_homepage/server/angular-app-manifest.mjs
@@ -24,12 +24,12 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 4845, hash: '18f8dc016fb734ddd8859b8a2ca2e35073a146d17707ea066b1b14d0cb350797', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1012, hash: '7dfc981e6a62c57c4f973fca7a9d268cd79980620d3473d6dce9c0940c444a59', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 9480, hash: 'daa4aa40640777d1b19b9aa5c6912635cf9ff88ceecdd7eb822aedc1e56a25d9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'fun-facts/index.html': {size: 7105, hash: 'b916f422b1f43961d84b6136706f7c0f697af69fb8c1d2d1ba222ef85ec88019', text: () => import('./assets-chunks/fun-facts_index_html.mjs').then(m => m.default)},
-    'data-security/index.html': {size: 6891, hash: 'c33143eeac4d361d2c8d76219724f8b96cc0a822a069f77d1829db666781edf0', text: () => import('./assets-chunks/data-security_index_html.mjs').then(m => m.default)},
-    'sand-site/index.html': {size: 7002, hash: 'a90d3dabc4cfe597cc6c42eaaccaf0b452c496360f2a7b9412b201ae5b739c64', text: () => import('./assets-chunks/sand-site_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 4845, hash: '8ccfd0e1471dc1602b4b76639dfd2fdec724fd429ce19dd983d248085e29267e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1012, hash: 'cfe60fa2a8708cbdfb9469bb8de32de329e9d5a36e9475dd86d8309968db95d3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'data-security/index.html': {size: 6891, hash: '1f1a97af8a834e37b41b0e79470165a755045692115ea767487393b554dcda31', text: () => import('./assets-chunks/data-security_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 9480, hash: '6af7b8ab63edf19e6dc519ba12689d43704b348eb742f4d821a8e1af58e52f2f', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'fun-facts/index.html': {size: 7105, hash: '88b0444f174523f90938304ab294fc4e74aa79bbfad4921f918f1d5efa49a285', text: () => import('./assets-chunks/fun-facts_index_html.mjs').then(m => m.default)},
+    'sand-site/index.html': {size: 7002, hash: '32b62207a6135b22ab83c19933ba941af84517a9e937ed6202b66ecd9618b3b8', text: () => import('./assets-chunks/sand-site_index_html.mjs').then(m => m.default)},
     'styles-WKIBCZ2A.css': {size: 4221, hash: 'X1vJgGzT4bM', text: () => import('./assets-chunks/styles-WKIBCZ2A_css.mjs').then(m => m.default)}
   },
 };
--- dist/Angular_personal_homepage/server/assets-chunks/sand-site_index_html.mjs
+++ dist/Angular_personal_homepage/server/assets-chunks/sand-site_index_html.mjs
@@ -10,6 +10,6 @@ export default `<!DOCTYPE html><html lang="en" data-beasties-container><head>
 <body><!--nghm--><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
 </script><script>window.__jsaction_bootstrap(document.body,"ng",["click"],[]);</script>
   <app-root ng-version="20.3.17" ngh="1" ng-server-context="ssg"><router-outlet></router-outlet><app-sand-site ngh="0"><div class="center"><button class="confetti-button" jsaction="click:;">Gelber Regen!</button><app-home-btn _nghost-ng-c512507282 ngh="0"><div _ngcontent-ng-c512507282><button _ngcontent-ng-c512507282 routerlink="/" tabindex="0" jsaction="click:;">Home</button></div></app-home-btn></div><div id="confetti"></div></app-sand-site><!----></app-root>
-<script src="polyfills-5CFQRCPP.js" type="module"></script><script src="main-QRXNPNGE.js" type="module"></script>
+<script src="polyfills-5CFQRCPP.js" type="module"></script><script src="main-CHIFLXN5.js" type="module"></script>
 
 <script id="ng-state" type="application/json">{"__nghData__":[{},{"c":{"0":[{"i":"c1913294096","r":1}]}}]}</script></body></html>`;
