bf2e618da2a5506079921feda7c31f6df0d7c7af
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-15 15:49:46 -0400

    add tests data-security for

---
+++ cypress/e2e/data-security.cy.ts
@@ -0,0 +1,20 @@
+describe('Site is acsesseble use', () => {
+  it('Site is acceseble', () => {
+    cy.visit('/data-security');
+  });
+});
+
+describe('Link Work and text is shown', () => {
+  it('Text is in page', () => {
+    cy.visit('/data-security');
+    cy.contains('Home');
+    cy.contains('Datenschutz');
+    cy.contains('Es werden keine Daten von dir gespeichert. Es werden auch keine Cookies verwendet oder andere Tracking-Technologien eingesetzt. Die Webseite ist komplett kostenlos und es gibt keine Werbung. ');
+  });
+  it('Link works', () => {
+    cy.visit('/data-security');
+    cy.contains('Home').click();
+    cy.url().should('include', '/');
+    cy.visit('/data-security');
+  });
+});
--- cypress/e2e/fun-facts.cy.ts
+++ cypress/e2e/fun-facts.cy.ts
@@ -12,5 +12,10 @@ describe('Link Work and text is shown', () => {
     cy.contains('Clicke den anderen Button um den Fackt zu sehen.');
     cy.contains('Home');
   });
+  it('Link works', () => {
+    cy.visit('/fun-facts');
+    cy.contains('Home').click();
+    cy.url().should('include', '/');
+    cy.visit('/fun-facts');
+  });
 });
-
