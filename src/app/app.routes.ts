5c6f39036cdc668cf63cc68b16d9e7c96e29b823
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-15 02:19:52 -0400

    add new path route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home';
 import { DataSecurityComponent } from './data-security/data-security';
 import { SandSite } from './sand-site/sand-site';
 import { FunFacts } from './fun-facts/fun-facts';
+import { TodoList } from './todo-list/todo-list';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'data-security', component: DataSecurityComponent },
   { path: 'sand-site', component: SandSite },
-  { path: 'fun-facts', component: FunFacts}
+  { path: 'fun-facts', component: FunFacts},
+  { path : 'todo-list', component: TodoList}
 ];
