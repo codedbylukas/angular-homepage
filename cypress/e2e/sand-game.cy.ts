372aea3f6abfd7811102211abf063da8c1b4b65b
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-15 15:27:20 -0400

    add test

--- cypress/e2e/sand-game.cy.ts
+++ cypress/e2e/sand-game.cy.ts
@@ -10,4 +10,10 @@ describe('Link Work and text is shown', () => {
     cy.contains('Gelber Regen!');
     cy.contains('Home');
   });
+  it('Link works', () => {
+    cy.visit('/sand-site');
+    cy.contains('Home').click();
+    cy.url().should('include', '/');
+    cy.visit('/sand-site');
+  });
 });
