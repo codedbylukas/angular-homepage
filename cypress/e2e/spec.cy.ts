274345705f6fcf6854cd4b6b1a0e635714b3ef36
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-15 14:27:46 -0400

    first test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,8 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running')
-  })
-})
+    cy.visit('/');
+    cy.contains('Hallo, ich bin Lukas');
+    cy.contains('Coole Webseiten');
+    cy.url().should('include', '/sand-site');
+  });
+});
