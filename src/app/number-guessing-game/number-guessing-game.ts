3601b4f9d29f95a0b37c6a756b588c5d2c33fee4
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-17 12:14:04 -0400

    add number-gessing-game

--- src/app/number-guessing-game/number-guessing-game.ts
+++ src/app/number-guessing-game/number-guessing-game.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-number-guessing-game',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './number-guessing-game.html',
   styleUrl: './number-guessing-game.scss',
 })
 export class NumberGuessingGame {
+  guess: number | null = null;
+  randomNumber: number = Math.floor(Math.random() * 100) + 1;
+  message: string = '';
+  attempts: number = 0;
+  gameWon: boolean = false;
 
+  checkGuess() {
+    if (this.guess === null) {
+      this.message = 'Bitte gib eine Zahl ein.';
+      return;
+    }
+
+    this.attempts++;
+
+    if (this.guess === this.randomNumber) {
+      this.message = 'Richtig! Du hast die Zahl erraten.';
+      this.gameWon = true;
+    } else if (this.guess < this.randomNumber) {
+      this.message = 'Deine Zahl ist zu niedrig. Versuche es noch einmal.';
+    } else {
+      this.message = 'Deine Zahl ist zu hoch. Versuche es noch einmal.';
+    }
+  }
+
+  resetGame() {
+    this.randomNumber = Math.floor(Math.random() * 100) + 1;
+    this.guess = null;
+    this.message = '';
+    this.attempts = 0;
+    this.gameWon = false;
+  }
 }
