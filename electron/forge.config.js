617efce9b8ae2e667e7fdbdd2d332fd35cf88a33
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-18 11:39:02 -0400

    fix

--- electron/forge.config.js
+++ electron/forge.config.js
@@ -4,33 +4,43 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    // Hier kannst du später ein Icon hinzufügen:
+    // icon: './path/to/icon' 
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        // Name der .exe für Windows
+        name: 'angular_homepage',
+      },
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin', 'linux'], // ZIP als Backup für Linux ist immer gut
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config: {
+        options: {
+          maintainer: 'Kali User',
+          homepage: 'https://deine-homepage.de',
+          // Verhindert Fehler bei fehlenden Icons oder Beschreibungen
+          description: 'Meine Angular Homepage App',
+          categories: ['Utility'],
+        },
+      },
     },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+    /* RPM wurde entfernt, da 'rpmbuild' auf Kali/Debian 
+      zu Berechtigungsfehlern mit der sqlite-Datenbank führt.
+    */
   ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
       config: {},
     },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
