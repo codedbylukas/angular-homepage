25e81c9aad347d2846379e06548245f815229e3f
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-18 10:55:18 -0400

    add tests

--- src/app/number-guessing-game/number-guessing-game.spec.ts
+++ src/app/number-guessing-game/number-guessing-game.spec.ts
@@ -27,4 +27,33 @@ describe('NumberGuessingGame', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should define check guess', () => {
+    expect(component.checkGuess).toBeDefined();
+    expect(typeof component.checkGuess).toBe('function');
+  });
+  it('should define resetGame', () => {
+    expect(component.resetGame).toBeDefined();
+    expect(typeof component.resetGame).toBe('function');
+  });
+  it('should define Variables', () => {
+    expect(component.guess).toBeDefined();
+    expect(component.randomNumber).toBeDefined();
+    expect(component.message).toBeDefined();
+    expect(component.attempts).toBeDefined();
+    expect(component.gameWon).toBeDefined();
+  });
+  it('should define Variables types', () => {
+    expect(typeof component.randomNumber).toBe('number');
+    expect(typeof component.message).toBe('string');
+    expect(typeof component.attempts).toBe('number');
+    expect(typeof component.gameWon).toBe('boolean');
+  });
+  it('should define Variables values', () => {
+    expect(component.guess).toBeNull();
+    expect(component.randomNumber).toBeGreaterThanOrEqual(1);
+    expect(component.randomNumber).toBeLessThanOrEqual(100);
+    expect(component.message).toBe('');
+    expect(component.attempts).toBe(0);
+    expect(component.gameWon).toBe(false);
+  });
 });
