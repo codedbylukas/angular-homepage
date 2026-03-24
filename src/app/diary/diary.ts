0d5a33077e16e80507e2d954cdf218c6df44f84c
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-24 13:14:51 -0400

    create diary

---
+++ src/app/diary/diary.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diary',
+  imports: [],
+  templateUrl: './diary.html',
+  styleUrl: './diary.scss',
+})
+export class Diary {
+
+}
