abff941fc74b1917026dbf244696f57f41e296ab
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-03-28 08:00:24 -0400

    add

--- src/app/diary/entry/entry.ts
+++ src/app/diary/entry/entry.ts
@@ -10,4 +10,8 @@ export class Entry {
   @Input() title: string = '';
   @Input() text: string = '';
   @Input() date: string = '';
+  @Input() id: number = 0;
+  del_object(id: number) {
+    localStorage.removeItem('diary' + id);
+  }
 }
