8e8442ecde00d23d697a9e556b8e0bbcdf5d19b8
codedbylukas <lukaswittek@gmail.com>
2026-05-02 08:04:57 -0500

    add more git leaks checks used

--- git-leaks.sh
+++ git-leaks.sh
@@ -1,3 +1,8 @@
 #/bin/bash
 
 gitleaks detect --verbose
+gitleaks dir . --verbose --no-git
+gitleaks protect --staged --verbose
+gitleaks detect --report-format sarif --report-path leaks.sarif
+gitleaks detect --report-format csv --report-path leaks.csv
+gitleaks detect --verbose --redact=0
