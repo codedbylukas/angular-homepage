788848b638ecb1c126478159f506fc1fd9a76d45
codedbylukas <lukaswittek@gmail.com>
2026-05-02 06:58:30 -0500

    add conf

--- src/app/diary/entry/entry.spec.ts
+++ src/app/diary/entry/entry.spec.ts
@@ -8,9 +8,8 @@ describe('Entry', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Entry]
-    })
-    .compileComponents();
+      imports: [Entry],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Entry);
     component = fixture.componentInstance;
@@ -19,5 +18,9 @@ describe('Entry', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('test if del_object is defined right', () => {
+    expect(component.del_object).toBeDefined();
+    expect(typeof component.del_object).toBe('function');
   });
 });
