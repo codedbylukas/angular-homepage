eb934d76ebc1215f76ba6cef0ce77fae647f87b3
codedbylukas <lukaswittek@gmail.com>
2026-05-02 07:07:33 -0500

    add more tests entry

--- src/app/diary/entry/entry.spec.ts
+++ src/app/diary/entry/entry.spec.ts
@@ -19,8 +19,29 @@ describe('Entry', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
   it('test if del_object is defined right', () => {
     expect(component.del_object).toBeDefined();
     expect(typeof component.del_object).toBe('function');
+  });
+
+  it('Defined variable date', () => {
+    expect(component.date).toBeDefined();
+  });
+
+  it('Defined variable id', () => {
+    expect(component.id).toBeDefined();
+  });
+
+  it('Defined variable text', () => {
+    expect(component.text).toBeDefined();
+  });
+
+  it('Defined variable title', () => {
+    expect(component.title).toBeDefined();
+  });
+
+  it('Id should have the right value(0)', () => {
+    expect(component.id).toBe(0);
   });
 });
